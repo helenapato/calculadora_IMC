e47c23a819aa858e7bb6daf24e62da3559ac11d9
helenapato <lenapato.m@gmail.com>
2022-01-06 20:11:44 -0300

    Testes e readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# calculadora_IMC
+# Calculadora de IMC
 
-Calcula o Índice de Massa corporal de adultos entre 18 e 60 anos. Pode usar o método tradicional ou o mais preciso.
+O índice de massa corporal (IMC) é uma medida internacional usada para calcular se uma pessoa está no peso ideal. Este sistema simples calcula o valor do IMC de adultos entre 18 e 60 anos. Isso porque o índice tradicional independente não é recomendado para avaliar crianças, adolescentes e idosos. A calculadora inclui também a opção de usar o método mais preciso do cálculo, proposto por Nick Trefethen.
+
+Para usar a calculadora basta fornecer sua massa (em quilogramas), altura (em metros) e idade (em anos inteiros). Para usar o método preciso, basta especificar a última entrada como True.
+
+Fonte:[Wikipedia](https://pt.wikipedia.org/wiki/%C3%8Dndice_de_massa_corporal)
--- tests/test_calculadora_imc.py
+++ tests/test_calculadora_imc.py
@@ -3,6 +3,53 @@ from src.calculadora_imc import calculadora_imc
 
 class TestCalculadoraIMC(unittest.TestCase):
     def testCriancaIMC(self):
-        resultado, imc = calculadora_imc(40, 1.6, 12)
+        resultado, imc = calculadora_imc(40, 1.6, 17)
         self.assertEqual(resultado, 'IMC inválido para crianças e adolescentes')
+        self.assertEqual(imc, 0)        
+
+    def testIdosoIMC(self):
+        resultado, imc = calculadora_imc(40, 1.6, 61)
+        self.assertEqual(resultado, 'IMC inválido para idosos')
         self.assertEqual(imc, 0)
+
+
+    def testMuitoAbaixoIMC(self):
+        resultado, imc = calculadora_imc(50, 1.75, 20)
+        self.assertEqual(resultado, 'Muito abaixo do peso')
+        self.assertAlmostEqual(imc, 16.33, delta=0.01)
+
+    def testAbaixoIMC(self):
+        resultado, imc = calculadora_imc(50, 1.65, 20)
+        self.assertEqual(resultado, 'Abaixo do peso')
+        self.assertAlmostEqual(imc, 18.37, delta=0.01)  
+
+    def testNormalIMC(self):
+        resultado, imc = calculadora_imc(60, 1.6, 20)
+        self.assertEqual(resultado, 'Peso normal')
+        self.assertAlmostEqual(imc, 23.44, delta=0.01)
+
+    def testAcimaIMC(self):
+        resultado, imc = calculadora_imc(85, 1.75, 20)
+        self.assertEqual(resultado, 'Acima do peso')
+        self.assertAlmostEqual(imc, 27.76, delta=0.01)         
+
+    def testObesaIMC(self):
+        resultado, imc = calculadora_imc(100, 1.7, 20)
+        self.assertEqual(resultado, 'Obesidade')
+        self.assertAlmostEqual(imc, 34.60, delta=0.01)
+
+    def testSeveraIMC(self):
+        resultado, imc = calculadora_imc(105, 1.65, 20)
+        self.assertEqual(resultado, 'Obesidade severa')
+        self.assertAlmostEqual(imc, 38.57, delta=0.01)
+
+    def testMorbidaIMC(self):
+        resultado, imc = calculadora_imc(120, 1.7, 20)
+        self.assertEqual(resultado, 'Obesidade mórbida')
+        self.assertAlmostEqual(imc, 41.52, delta=0.01)
+
+
+    def testPrecisoIMC(self):
+        resultado, imc = calculadora_imc(120, 1.7, 20, True)
+        self.assertEqual(resultado, 'Obesidade mórbida')
+        self.assertAlmostEqual(imc, 41.40, delta=0.01)
